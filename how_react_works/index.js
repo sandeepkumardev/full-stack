d772b1e70245615c5d91934f4f7e01043962f019
Nitish <nitish.johri@gmail.com>
2024-10-22 12:25:02 +0530

    react start

---
+++ how_react_works/index.js
@@ -0,0 +1,12 @@
+ document.getElementById("yo").innerHTML ="Hello World";
+ const div =document.createElement("div");
+ div.innerHTML ="container";
+
+
+ const h1 = document.createElement("h1");
+ h1.innerHTML ="hello world";
+
+ const root = document.getElementById("root");
+ root.appendChild(h1);
+ root.appendChild(div);
+
