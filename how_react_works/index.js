d4a1081b8c4f92a775e99cbe416949d70d6c37f2
Jalal Noushad Khan <jalalnoushadkhan4087@gmail.com>
2024-10-25 08:55:06 +0530

    picture added

--- how_react_works/index.js
+++ how_react_works/index.js
@@ -2,7 +2,7 @@
 // h1.innerHTML = "hello from js";
 // const root = document.getElementById("root");
 const h1 = document.getElementById("h1")
-h1.innerHTML = "HELLO from js";
+h1.innerHTML = "hello 1";
 const div = document.createElement("div");
 div.appendChild(h1);
 const root = document.getElementById("root");
