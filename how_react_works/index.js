6d4ae8c6a68d5a7457385b79eeb5164857fa8a4c
Ayush Saini <ayushsaini2601@gmail.com>
2024-10-25 22:22:24 +0530

    learn new basics

---
+++ how_react_works/index.js
@@ -0,0 +1,13 @@
+const h2 = document.createElement('h2');
+h2.innerText = "Hello from JS"
+
+const Ayush = document.getElementById("Ayush")
+Ayush.appendChild(h2)
+
+// const p = document.getElement('p');
+// p.innertext="HEllo Everyone"
+
+// const new =document.getElementById(''new'');
+// new.appendChild(p)
+
+
