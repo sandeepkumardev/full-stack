92bb249ff18a26752368017d4a6072b206595057
sandeep kumar <sandeepypb@gmail.com>
2024-10-28 10:15:22 +0530

    ✨ Feat: write components

--- basic_react/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const heading = React.createElement("h1", null, "Hello from React");
-const newDiv = React.createElement("div", null, heading);
-const reactDiv = React.createElement("div", null, [newDiv]);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(reactDiv);
---
+++ basic_react/src/components/Main.js
@@ -0,0 +1,14 @@
+const Main = () => {
+  return (
+    <div className="main">
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+const Card = () => {
+  return <div className="card">Item info</div>;
+};
+
+export default Main;
