38a06e1a065ee49dce471f0594549b23a5c508ab
Ayush Saini <ayushsaini2601@gmail.com>
2024-10-30 10:00:02 +0530

    use useState in  counter

---
+++ 04 react_by_topics/src/App.js
@@ -0,0 +1,11 @@
+import Counter from "./Features/Counter/Counter";
+import "./App.css"
+
+const App=()=> {
+    return(
+        <div>
+            <Counter/>
+        </div>
+    )
+};
+export default App;
---
+++ 04 react_by_topics/src/Features/Counter/Counter.js
@@ -0,0 +1,30 @@
+import {useState} from "react";
+
+const Counter=()=>{
+const [count,setCount]= useState(0);
+
+function increment() {
+    // if (count>=0){
+    // setCount(count+1); }
+    setCount(count+1);
+}
+
+function decreament() {
+    if(count>0){
+    setCount(count-1);}
+    // setCount(count-1);
+}
+function reset(){
+    setCount(0);
+}
+
+    return(
+        <div className="in">
+            <h1>{count}</h1>
+            <button onClick={increment}>+</button>
+            <button onClick={decreament}>-</button>
+            <button onClick={reset}>reset</button>
+        </div>
+    )
+}
+export default Counter;
