47b3751032479a2289423b4f06ec595faaa696d3
jalal <jalalnoushadkhan4087@gmail.com>
2024-11-13 10:35:39 +0530

    updated code

--- 04todo/src/App.js
+++ 04todo/src/App.js
@@ -1,26 +1,78 @@
 import { useState } from "react";
 
-const App = () => {
-    const [input, setInput] = useState("");
-    const [todos, setTodos] = useState([]);
-    const handleInputchange = (event) => {
-        setInput(event.target.value);
-    };
-    const handleAdd = () => {
-        setTodos([input, ...todos]);
-        setInput("");
-    };
-    return(
-        <div>
-            <input type="text" value={input} onchange= {handleInputchange=}/>
-            <button onClick={handleAdd}>ADD</button>
-            <ul>
-                {todos.map((item, index) => (
-                    <li key={index}>{index}></li>
+const ToDo = () => {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [showInputIndex, setShowInputIndex] = useState(null);
+  const [editInput, setEditInput] = useState("");
 
-                )}
-        
-        </div>
-    )
-}
-export default App
+  const handleShowInput = (index) => {
+    setShowInputIndex(index);
+  };
+
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+
+    if (!input || !input.trim()) return;
+
+    setTodos([input.trim(), ...todos]);
+    setInput("");
+  };
+
+  const handleDelete = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  const handleInputUpdate = (index) => {
+    setTodos(todos.map((item, i) => (index === i ? editInput : item)));
+    setShowInputIndex(null);
+    setEditInput("");
+  };
+  return (
+    <div>
+      <form onSubmit={handleAdd}>
+        <input type="text" value={input} onChange={handleInputChange} />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((item, index) => (
+          <div key={index} className="todo">
+            {showInputIndex === index ? (
+              <input
+                type="text"
+                value={editInput}
+                onChange={(event) => setEditInput(event.target.value)}
+                className="inputbox"
+              />
+            ) : (
+              <li>{item}</li>
+            )}
+
+            <div>
+              {showInputIndex === index ? (
+                <button onClick={() => handleInputUpdate(index)}>Update</button>
+              ) : (
+                <button
+                  onClick={() => {
+                    handleShowInput(index);
+                    setEditInput(item);
+                  }}
+                >
+                  Edit
+                </button>
+              )}
+
+              <button onClick={() => handleDelete(index)}>Delete</button>
+            </div>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ToDo;
