f8db637e80f428dd65b9cf4e0f5706c7ba79e483
sandeep kumar <sandeepypb@gmail.com>
2024-11-25 10:40:42 +0530

    ✨ Feat: basic css

---
+++ projects/react_styles/src/App.jsx
@@ -0,0 +1,45 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      {/* <div className="box">Hello</div> */}
+
+      {/* <div className="fonts">
+        <h1>Hello</h1>
+        <p>Paragraph</p>
+      </div> */}
+
+      {/* <div className="layout">
+        <span></span>
+        <span></span>
+      </div> */}
+
+      <div className="pos">
+        <div className="box"></div>
+      </div>
+    </div>
+  );
+};
+
+// class
+// id
+// element name
+
+export default App;
+
+// inline css
+// css file
+// css modules
+
+// flex
+// grid
+// position
+
+// media queries
+// animations
+// transitions
+// pseudo selectors
+// pseudo elements
+// variables
+// sass
