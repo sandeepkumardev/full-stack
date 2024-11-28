f508ad643e1146eefe767c325ea1d9979c9691f1
sandeep kumar <sandeepypb@gmail.com>
2024-11-28 11:22:59 +0530

    ✨ Feat: add not found page

--- projects/auth/src/pages/NotFound.jsx
+++ projects/auth/src/pages/NotFound.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-  return <>NotFound</>;
+  return (
+    <div className="not_found">
+      <h1>404</h1>
+      <p>Page Not Found</p>
+      <Link to="/">Home</Link>
+    </div>
+  );
 };
 
 export default NotFound;
