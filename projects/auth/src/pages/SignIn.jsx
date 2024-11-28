71c79415066b626e700b51af459cf6b326e40383
sandeep kumar <sandeepypb@gmail.com>
2024-11-28 11:13:23 +0530

    ✨ Feat: add auth screens

--- projects/auth/src/pages/SignIn.jsx
+++ projects/auth/src/pages/SignIn.jsx
@@ -1,5 +1,37 @@
+import { Link, useNavigate } from "react-router-dom";
+
 const SignIn = () => {
-  return <>SignIn</>;
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // sign in logic
+    // save user info
+
+    navigate("/");
+  };
+  return (
+    <div className="auth_container">
+      <div className="auth_form">
+        <form onSubmit={handleSubmit}>
+          <div className="input_control">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="Email" />
+          </div>
+          <div className="input_control">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder="Email" />
+          </div>
+
+          <input type="submit" value="Sign In" className="submit_btn" />
+        </form>
+        <p className="signup_link">
+          Don&apos;t have an account? <Link to={"/sign-up"}>Sign Up</Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
--- projects/auth/src/pages/SignUp.jsx
+++ projects/auth/src/pages/SignUp.jsx
@@ -1,5 +1,45 @@
+import { Link, useNavigate } from "react-router-dom";
+
 const SignUp = () => {
-  return <>SignUp</>;
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // sign up logic
+    // save user info
+
+    navigate("/");
+  };
+  return (
+    <div className="auth_container">
+      <div className="auth_form">
+        <form onSubmit={handleSubmit}>
+          <div className="input_control">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" placeholder="Name" />
+          </div>
+          <div className="input_control">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="Email" />
+          </div>
+          <div className="input_control">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder="Password" />
+          </div>
+          <div className="input_control">
+            <label htmlFor="confirm_password">Confirm Password</label>
+            <input type="password" id="confirm_password" placeholder="Confirm Password" />
+          </div>
+
+          <input type="submit" value="Sign Up" className="submit_btn" />
+        </form>
+        <p className="signup_link">
+          Already have an account? <Link to={"/sign-in"}>Sign In</Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
